c0c94058d3301295c8ee4026c7140c0047b21797
t0yosi <177050929+t0yosi@users.noreply.github.com>
2024-09-08 14:48:48 +0100

    nav fix

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,23 +27,23 @@ const Header = () => {
   }, [location.search]);
   return (
     // <header className="border-b border-white md:border-none sticky top-0 z-10 backdrop-blur ">
-    <header className="sticky top-0 z-10 backdrop-blur ">
+    <header className="sticky top-0 z-10 backdrop-blur">
       <nav className="py-auto bg-transparent">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div className="relative flex flex-col sm:flex-row sm:justify-between h-auto sm:h-16 items-center">
-            <div className="flex items-center justify-between w-full sm:w-auto">
+          <div className="relative flex flex-col md:flex-row md:justify-between h-auto sm:h-16 items-center">
+            <div className="flex items-center justify-between w-full md:w-auto">
               <div className="flex items-center gap-3">
                 <img
-                  className="h-8 w-auto"
+                  className="h-8 w-auto min-w-8"
                   src="https://tailwindui.com/img/logos/mark.svg?color=purple&shade=800"
                   alt="Terra List"
                 />
                 <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-                  <span className="text-orange-500">Terra</span>
-                  <span className="text-violet-500">List</span>
+                  <span className="text-orange-500 md:hidden">Terra</span>
+                  <span className="text-violet-500 md:hidden">List</span>
                 </h1>
               </div>
-              <div className="flex items-center sm:hidden  ">
+              <div className="flex items-center md:hidden">
                 <button
                   type="button"
                   className="relative inline-flex items-center justify-center border border-violet-400 rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
@@ -86,7 +86,7 @@ const Header = () => {
                 </button>
               </div>
             </div>
-            <div className="hidden sm:ml-6 sm:flex items-center space-x-4">
+            <div className="hidden md:flex items-center space-x-4">
               <Link to="/">
                 <div
                   className={`${
@@ -124,15 +124,15 @@ const Header = () => {
                 </div>
               </Link>
             </div>
-            <div className="flex flex-col sm:flex-row items-center justify-between w-full sm:w-auto mb-3 sm:mb-0">
+            <div className="flex flex-col md:flex-row items-center justify-between w-full md:w-auto">
               <form
                 onSubmit={handleSubmit}
-                className="bg-slate-100 border border-purple-600 p-3 rounded-lg flex items-center h-7 mt-3 sm:mt-0 sm:ml-auto w-full"
+                className="bg-slate-100 border border-purple-600 p-3 rounded-lg flex items-center h-7 mt-3 md:mt-0 md:ml-auto w-full"
               >
                 <input
                   type="text"
                   placeholder="search..."
-                  className="bg-transparent focus:outline-none w-full sm:w-64 md:w-44"
+                  className="bg-transparent focus:outline-none w-full md:w-36 lg:w-72"
                   value={searchTerm}
                   onChange={(e) => setSearchTerm(e.target.value)}
                 />
@@ -140,7 +140,7 @@ const Header = () => {
                   <FaSearch className="text-slate-500 align-text-right" />
                 </button>
               </form>
-              <div className="hidden sm:flex">
+              <div className="hidden md:flex">
                 {currentUser ? (
                   <LoggedInView user={currentUser} />
                 ) : (
@@ -152,7 +152,7 @@ const Header = () => {
         </div>
 
         <div
-          className={`${isMobileMenuOpen ? "block" : "hidden"} sm:hidden`}
+          className={`${isMobileMenuOpen ? "block" : "hidden"} md:hidden`}
           id="mobile-menu"
         >
           <div className="space-y-1 px-2 pb-3 pt-2">
@@ -190,11 +190,7 @@ const Header = () => {
                 Search
               </div>
             </Link>
-            {currentUser ? (
-              <MobileViewOptions/>
-            ) : (
-              <LoggedOutView />
-            )}
+            {currentUser ? <MobileViewOptions /> : <LoggedOutView />}
           </div>
         </div>
       </nav>
